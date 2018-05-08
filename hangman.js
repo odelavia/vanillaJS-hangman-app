4cc90555ff8d2c67cdea8597dfd533c3774b36f9
Oliver De La Via <odelavia@gmail.com>
2018-05-08 13:56:03 -0400

    initial commit

---
+++ hangman.js
@@ -0,0 +1,22 @@
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['t']
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+    return puzzle
+}
+
+const game1 = new Hangman('tomato', 5)
+console.log(game1.getPuzzle())
+const game2 = new Hangman('helium', 4)
+console.log(game2.getPuzzle())
